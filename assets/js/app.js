f3705ea6f33c61c42ec50e11fe90a6d41b12ed98
julien-gabard <juliengabard@hotmail.fr>
2021-03-12 11:15:42 +0100

    construction of object oriented JS and addition of methods

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+let app = {
+
+  display: '0',
+
+  /**
+   * Method executed when launching the application. ( constructor )
+   */
+  init: () => {
+
+  },
+
+  /**
+   * Method which combines the method of buttons and display.
+   * @param {event} evt 
+   */
+  playButtonAndRefreshDisplay: (evt) => {
+    // TODO
+  },
+
+  /**
+   * Method to display current operation.
+   */
+  displayCurrentOperation: () => {
+    // TODO
+  },
+
+  /**
+   * Method to display result.
+   */
+  displayResult: () => {
+    // TODO
+  },
+
+  /**
+   * Method used to manage the listening of the click on a button.
+   * @param {event} evt
+   */
+  buttonClickManagement: (evt) => {
+
+  },
+
+  /**
+   * Method for performing an action based on the button clicked.
+   * @param {string} button
+   */
+  calculate: (button) => {
+    // TODO
+  }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
